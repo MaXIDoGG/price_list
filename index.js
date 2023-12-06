0558d22e468f6356bcb1b963de29109e06128f35
Maxim Nasonov <maks.nasonov.03@gmail.com>
2023-12-06 20:59:14 +0700

    1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // подключение модулей
 const express = require("express");
+const path = require('path')
 const { v4: uuidv4 } = require('uuid');
 const db = require('./db.js')
 const Products = require('./models/products.js');
@@ -14,20 +15,36 @@ db.authenticate()
 // определяем обработчик для маршрута "/"
 app.get("/", async (req, res) => {
     try {
-        await Products.create({
-            id: uuidv4(),
-            name: "Кроссовки Nike",
-            category: "Спортивная обувь",
-            price: 3000
-        })
+        // await Products.create({
+        //     id: uuidv4(),
+        //     name: "Кроссовки Nike",
+        //     category: "Спортивная обувь",
+        //     price: 3000
+        // })
+        res.sendFile(path.join(__dirname, 'views/index.html'));
 
     } catch (error) {
         res.status(400).json({
             error: error.message
         })
-        console.log(UUID())
     }
 
 });
+
+app.get("/searchProduct", async (req, res) => {
+    try {
+        console.log(req.params)
+        let products = await Products.findAll();
+        res.send(products)
+    } catch (error) {
+        res.status(400).json({
+            error: error.message
+        })
+    }
+})
+
+// app.post("/addCategory") {
+
+// }
 // начинаем прослушивать подключения на 3000 порту
 app.listen(3000);
