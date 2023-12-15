0c08cc6171107e692681d199b04cfd0d3814b967
Maxim Nasonov <maks.nasonov.03@gmail.com>
2023-12-15 19:32:42 +0700

    haha1

--- db.js
+++ db.js
@@ -1,16 +1,3 @@
 const Sequilize = require('sequelize')
 
-module.exports = new Sequilize("postgresql://maks.nasonov.03:4bKdyM3zXASe@ep-sweet-brook-09241647.eu-central-1.aws.neon.tech/interfaces?sslmode=require")
-//     'interfaces', 'maks.nasonov.03', '4bKdyM3zXASe', {
-//     host: 'ep-sweet-brook-09241647.eu-central-1.aws.neon.tech',
-//     dialect: 'postgres',
-//     sslmode: "require"
-// }
-
-
-// # Do not expose your Neon credentials to the browser
-
-// PGHOST='ep-sweet-brook-09241647.eu-central-1.aws.neon.tech'
-// PGDATABASE='interfaces'
-// PGUSER='maks.nasonov.03'
-// PGPASSWORD='4bKdyM3zXASe'
+module.exports = new Sequilize("connection_string")
